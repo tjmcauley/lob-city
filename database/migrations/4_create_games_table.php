12dadd078186e57f364a3846fff438b60f51b662
tjmcauley <tjmcauley1234@gmail.com>
2024-10-28 15:30:13 +0000

    added fields to games migration table

--- database/migrations/4_create_games_table.php
+++ database/migrations/4_create_games_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('home_team_id')->unsigned();
+            $table->bigInteger('away_team_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('home_team_id')->references('id')->on('teams')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('away_team_id')->references('id')->on('teams')
+                ->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
