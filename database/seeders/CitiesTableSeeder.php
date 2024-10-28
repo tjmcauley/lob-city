48a6eeebc71cc4b43998693290e335f1d5e53e4c
tjmcauley <tjmcauley1234@gmail.com>
2024-10-28 16:03:11 +0000

    Added CitiesTableSeeder and updated DatabaseSeeder to seed values into the Cities table

---
+++ database/seeders/CitiesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\City;
+
+class CitiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $c1 = new City;
+        $c1->name = "Los Angeles";
+        $c1->save();
+    }
+}
