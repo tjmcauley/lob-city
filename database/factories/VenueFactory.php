79f60f8819b5c4580a35c67ef280c5a8f66b1e4b
tjmcauley <tjmcauley1234@gmail.com>
2024-10-31 17:25:29 +0000

    Coursework 1 submission-worthy

--- database/factories/VenueFactory.php
+++ database/factories/VenueFactory.php
@@ -17,12 +17,14 @@ class VenueFactory extends Factory
      */
     public function definition(): array
     {
-        $num_of_teams = DB::table('teams')->count(); // Number of rows in teams table
-        $num_of_cities = DB::table('cities')->count(); // Number of rows in cities table
-        $venue_names = ['Center', 'Arena', 'Forum', 'Garden', 'Fieldhouse', 'Dome'];
+        $num_of_cities = DB::table('cities')->count();
+        $venue_names = ['Center', 'Arena', 'Forum', 'Garden', 
+            'Fieldhouse', 'Dome'];
+        
         return [
-            'name' => fake()->company() . ' ' . fake()->randomElement($venue_names),
-            'city_id' => fake()->NumberBetween(1, $num_of_cities), // ID for the city a venue belongs to
+            'name' => fake()->company() . ' ' 
+                . fake()->randomElement($venue_names),
+            'city_id' => fake()->NumberBetween(1, $num_of_cities),
         ];
     }
 }
