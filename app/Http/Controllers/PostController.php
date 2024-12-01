88a26141a197de4d15865044d1aab5424c35424d
tjmcauley <tjmcauley1234@gmail.com>
2024-12-01 02:22:35 +0000

    Added views for posts and ability to make posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $p = new Post;
+        $p->user_id = $request->user()['id'];
+        $p->image_name = $request['image'];
+        $p->caption = $request['caption'];
+        $p->likes = 0;
+        $p->save();
+
+        session()->flash('message', 'post was uploaded!');
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $post->caption }}
+        </h2>
+    </x-slot>
+
+    <body class="font-sans text-gray-900 antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            <!-- Session Status -->
+            @can('admin')
+            <form method="POST" action="{{ route('teams.destroy', ['team' => $team]) }}">
+                @csrf
+                @method("DELETE")
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button class="ms-3">
+                        {{ __('Delete') }}
+                    </x-primary-button>
+                </div>
+            </form>
+            @endcan
+        </div>
+    </body>
+</x-app-layout>
