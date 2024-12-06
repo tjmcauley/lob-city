4c8e2e4e36be915fca02c58da27a9ad62c473ce0
tjmcauley <tjmcauley1234@gmail.com>
2024-12-06 15:00:59 +0000

    Used tailwind layout in create-post livewire component

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -5,13 +5,14 @@
     </div>
 
     <!-- Image -->
-    <div class="mt-4 text-black">
+    <div class="mt-4 text-white">
         <input wire:model="image" name="image" class="block mt-1 w-full" type="file" />
     </div>
 
     <!-- Create Post Button-->
-    <div class="flex items-center justify-end mt-4">
-        <button class="ms-3" wire:click="create_post" type="submit">Post</button>
+    <div
+        class="flex items-center justify-end mt-4 bg-black text-white py-2 px-4 rounded-lg hover:bg-gray-800 focus:outline-none">
+        <button class="" wire:click="create_post" type="submit">Post</budivn>
     </div>
 
     <ul>
@@ -30,9 +31,9 @@
                 <img src="{{ asset('/storage/' . $post->image_name) }}" />
             </div>
             <div class="text-white p-4 rounded-lg shadow-lg max-w-2xl mx-auto">
-            @foreach ($post->comments as $comment)
-            <h2>"{{ $comment->content }}"</h2>
-            @endforeach
+                @foreach ($post->comments as $comment)
+                <h2>"{{ $comment->content }}"</h2>
+                @endforeach
             </div>
         </li>
         <div class="flex justify-center items-center space-x-4 p-4">
