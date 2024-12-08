1625c6bd9af950ee806d67f825e16c122fbfb301
tjmcauley <tjmcauley1234@gmail.com>
2024-12-08 14:03:51 +0000

    Added default tags based off migrations

--- database/seeders/TagsTableSeeder.php
+++ database/seeders/TagsTableSeeder.php
@@ -5,7 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Tag;
+use App\Models\City;
+use App\Models\Venue;
+use App\Models\Team;
+use App\Models\Player;
 
 class TagsTableSeeder extends Seeder
 {
@@ -14,14 +19,34 @@ class TagsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $t1 = new Tag;
-        $t1->name = "LA Lakers";
-        $t1->save();
-        $t1->posts()->attach(1);
+        $cities = City::all();
+        $venues = Venue::all();
+        $teams = Team::all();
+        $players = Player::all();
 
-        $t2 = new Tag;
-        $t2->name = "Lebron James";
-        $t2->save();
-        $t2->posts()->attach(1);
+        foreach ($cities as $city) {
+            $t = new Tag;
+            $t->name = $city->name;
+            $t->save();
+        }
+
+        foreach ($venues as $venue) {
+            $t = new Tag;
+            $t->name = $venue->name;
+            $t->save();
+        }
+
+        foreach ($teams as $team) {
+            $t = new Tag;
+            $t->name = $team->name;
+            $t->save();
+        }
+
+        foreach ($players as $player) {
+            $t = new Tag;
+            $t->name = $player->name;
+            $t->save();
+        }
+
     }
 }
