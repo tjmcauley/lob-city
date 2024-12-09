1218a7147cb9dce5fdf29916de4c7db2eb5d60d3
tjmcauley <tjmcauley1234@gmail.com>
2024-12-09 12:08:09 +0000

    Made show consistent across post and all other filterable models

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Players') }}
+            {{ __('Filter posts by players') }}
         </h2>
 
         <div class="py-12">
