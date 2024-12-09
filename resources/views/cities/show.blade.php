8b2391315f45805598b62124863b31c978c3fe0c
tjmcauley <tjmcauley1234@gmail.com>
2024-12-09 13:41:07 +0000

    Updated views to remove edit/delete option

--- resources/views/cities/show.blade.php
+++ resources/views/cities/show.blade.php
@@ -10,21 +10,14 @@
             @foreach ($posts as $post)
             @foreach ($post->tags as $tag)
             @if ($tag->name === $city->name)
-
-            <div
-                class="text-white text-lg font-semibold text-center bg-gray-800 p-4 rounded-lg shadow-lg max-w-md mx-auto">
-                <a href="{{ route('posts.show', $post->user) }}">
-                    <h1> <u>{{ $post->user->email }}</u> </h1> posted on - {{$post->created_at->format('m-d H:i')}}
-                </a>
-                <h2> {{ $post->caption }} </h2>
-            </div>
-
-            <div class="flex justify-center items-center space-x-4 p-4">
+            <div>
+                <div
+                    class="text-white text-lg font-semibold text-center bg-gray-800 p-4 rounded-lg shadow-lg max-w-md mx-auto">
+                    <h2> posted on - {{$post->created_at->format('m-d H:i')}} </h2>
+                    <h2> {{ $post->caption }} </h2>
+                </div>
                 <img src="{{ asset('/storage/' . $post->image_name) }}" />
-            </div>
-
-            <div class="text-lg font-semibold text-white">
-                <div class="text-white p-4 rounded-lg shadow-lg max-w-2xl mx-auto">
+                <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
                     <livewire-create-comment :post="$post" />
                 </div>
             </div>
